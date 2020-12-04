71f5a070838ac609b93293cbe1ad6a65fd948af9
Kattyq <katiagorislavko@mail.ru>
2020-12-04 13:23:11 +0300

    Home_1

---
+++ scripts_2.js
@@ -0,0 +1,29 @@
+let width = 10,
+    heigh = 23 ,
+    SPryam = width * heigh + 'см';
+console.log(SPryam);
+
+let radius = 5,
+    SKruga = Math.PI * radius * radius;
+console.log(SKruga);
+
+let a = 5,
+    b = 7,
+    h = 10,
+    STrap = 1/2 * (a + b) * h;
+console.log(STrap);
+
+let S = 2000000,
+    p = 0.1,
+    years = 5,
+    Pereplata = S * p * years + 'руб.';
+console.log(Pereplata);
+
+let A = 8,
+    B = 3,
+    x1 = (16 - A + B) / 2,
+    x2 = (A - 15 * B) / (B - 6),
+    x3 = 23780 / (1 + 2 + A + B);
+console.log(x1);
+console.log(x2);
+console.log(x3);
